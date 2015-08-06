1df25d3f01112d853e02ad167b2ff210b3a045a8
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-08-06 19:28:04 +0200

    gulp watcher

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require( "gulp" );
 var stylus = require( "gulp-stylus" );
+var batch = require( "gulp-batch" );
+var watch = require( "gulp-watch" );
 
 gulp.task( "default", [ "styl" ] );
 
@@ -7,4 +9,10 @@ gulp.task( "styl", function () {
     gulp.src( "./src/main/webapp/resources/stylus/**/*.styl" )
         .pipe(stylus())
         .pipe( gulp.dest( "./src/main/webapp/resources/css" ) );
+});
+
+gulp.task("watch", function () {
+    return watch( "./src/main/webapp/resources/stylus/**/*.styl", batch( function ( events, done ) {
+        gulp.start( "styl", done ).pipe( "watch" );
+    }));
 });

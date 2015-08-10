e8a171a3308490bf5e09a8afb730ebcd6bba729b
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-08-10 10:37:48 +0200

    Migration for api_queue.pending_request table

---
+++ src/main/java/db/migration/V8__Create_Api_queue_pending_requests.java
@@ -0,0 +1,31 @@
+package db.migration;
+
+import org.flywaydb.core.api.migration.jdbc.JdbcMigration;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+/**
+ * Create "pending_requests" table in "api_queue" schema.
+ */
+public class V8__Create_Api_queue_pending_requests implements JdbcMigration {
+    public void migrate(Connection connection) throws Exception {
+        PreparedStatement statement = connection.prepareStatement(
+            "CREATE SCHEMA api_queue;" +
+            "CREATE table api_queue.pending_requests(" +
+            "    id serial," +
+            "    executor character varying(255)," +
+            "    path_params json," +
+            "    query_params json," +
+            "    priority integer NOT NULL DEFAULT 0," +
+            "    github_id integer references github.users(github_id)," +
+            "    CONSTRAINT api_queue_pending_requests_pkey PRIMARY KEY(id)" +
+            ");"
+        );
+
+        try {
+            statement.execute();
+        } finally {
+            statement.close();
+        }
+    }
+}

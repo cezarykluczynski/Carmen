200f20fd3a258daadbbef871879c5d03422abbd2
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-08-20 22:16:56 +0200

    Test Cassandra column names on Travis

--- src/main/java/com/cezarykluczynski/carmen/db/CassandraMigrations.java
+++ src/main/java/com/cezarykluczynski/carmen/db/CassandraMigrations.java
@@ -2,21 +2,28 @@ package com.cezarykluczynski.carmen.db;
 
 import com.netflix.astyanax.*;
 import com.netflix.astyanax.impl.*;
+import com.netflix.astyanax.model.Column;
 import com.netflix.astyanax.model.ColumnFamily;
 import com.netflix.astyanax.model.CqlResult;
+import com.netflix.astyanax.model.Rows;
 import com.netflix.astyanax.serializers.StringSerializer;
 import com.netflix.astyanax.serializers.UUIDSerializer;
 import com.netflix.astyanax.connectionpool.*;
 import com.netflix.astyanax.connectionpool.impl.*;
 import com.netflix.astyanax.thrift.ThriftFamilyFactory;
+import com.netflix.astyanax.util.RangeBuilder;
+import com.netflix.astyanax.thrift.model.ThriftRowImpl;
+import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
+import com.netflix.astyanax.model.ColumnList;
 
 import com.google.common.collect.ImmutableMap;
-import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.sql.Date;
 import java.util.Properties;
 import java.util.UUID;
+import java.util.Iterator;
 
 class CassandraMigrations {
 
@@ -39,8 +46,6 @@ class CassandraMigrations {
             cluster = properties.getProperty("cassandra.cluster");
             keyspaceName = properties.getProperty("cassandra.keyspace");
             version = (String) properties.getProperty("cassandra.version");
-
-            System.out.println(version);
         } catch (IOException ioe) {
             System.out.println("Carmen: cassandra.properties not found or incomplete.");
 
@@ -52,7 +57,7 @@ class CassandraMigrations {
             .forKeyspace(keyspaceName)
             .withAstyanaxConfiguration(new AstyanaxConfigurationImpl()
                 .setTargetCassandraVersion(version)
-                .setCqlVersion("3.0.0")
+                .setCqlVersion("3.1.1")
                 .setDiscoveryType(NodeDiscoveryType.NONE)
             )
             .withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("MyConnectionPool")
@@ -68,6 +73,7 @@ class CassandraMigrations {
 
         createKeyspace(keyspace);
         createMigrationsTable(keyspace);
+        migrate(keyspace);
 
         context.shutdown();
         System.exit(0);
@@ -109,8 +115,56 @@ class CassandraMigrations {
         OperationResult<CqlResult<UUID, String>> result
             = keyspace.prepareQuery(SCHEMA_MIGRATIONS)
                 .withCql("CREATE TABLE IF NOT EXISTS " + keyspaceName  +".schema_migrations " +
-                    "(id uuid, name varchar, created timestamp, PRIMARY KEY(id));")
+                    "(version int, name varchar, created timestamp, PRIMARY KEY(version));")
                 .execute();
+    }
+
+    private static void migrate(Keyspace keyspace) throws ConnectionException {
+        ColumnFamily<String, String> SCHEMA_MIGRATIONS = ColumnFamily.newColumnFamily(
+                "schema_migrations",
+                StringSerializer.get(),
+                StringSerializer.get()
+            );
+
+        Rows<String, String> rows = keyspace.prepareQuery(SCHEMA_MIGRATIONS)
+            .getAllRows()
+            .setBlockSize(10)
+            .withColumnRange(new RangeBuilder().setMaxSize(10).build())
+            .execute().getResult();
+
+        Iterator iterator = rows.iterator();
+
+        String columnName;
+        Integer version;
+        String name;
+        java.util.Date created;
+
+        while(iterator.hasNext()) {
+            ThriftRowImpl row = (ThriftRowImpl) iterator.next();
+            ColumnList columns = row.getColumns();
+            Iterator columnsIterator = columns.iterator();
+
+            columnName = null;
+            version = null;
+            name = null;
+            created = null;
+
+            while(columnsIterator.hasNext()) {
+                Column column = (Column) columnsIterator.next();
+                columnName = (String) column.getName();
+                System.out.println("columnName: '" + columnName + "'");
+                if (columnName.equals("version")) {
+                    version = column.getIntegerValue();
+                } else if (columnName.equals("name")) {
+                    name = column.getStringValue();
+                } else if (columnName.equals("created")) {
+                    created = column.getDateValue();
+                }
+            }
+            System.out.println(version);
+            System.out.println(name);
+            System.out.println(created);
+        }
     }
 
 }

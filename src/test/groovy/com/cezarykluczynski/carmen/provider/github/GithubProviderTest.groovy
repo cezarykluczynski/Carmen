03cabae0f0c658729f70428f7c4627eea2974703
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-09-23 19:42:03 +0200

    More tests for GithubProvider

--- src/test/groovy/com/cezarykluczynski/carmen/provider/github/GithubProviderTest.groovy
+++ src/test/groovy/com/cezarykluczynski/carmen/provider/github/GithubProviderTest.groovy
@@ -12,10 +12,11 @@ import static org.mockito.Mockito.mock
 import static org.mockito.Mockito.when
 import static org.mockito.Mockito.verify
 
-import com.cezarykluczynski.carmen.set.github.User
+import com.cezarykluczynski.carmen.set.github.User as UserSet
 import com.cezarykluczynski.carmen.set.github.RateLimit
 import com.cezarykluczynski.carmen.util.PaginationAwareArrayList
 import com.cezarykluczynski.carmen.dao.github.RateLimitDAOImpl
+import com.cezarykluczynski.carmen.dao.github.UserDAOImplFixtures
 
 import java.io.IOException
 import java.util.Iterator
@@ -23,9 +24,13 @@ import java.util.Iterator
 @ContextConfiguration([
     "classpath:spring/database-config.xml",
     "classpath:spring/mvc-core-config.xml",
-    "classpath:spring/cron-config.xml"
+    "classpath:spring/cron-config.xml",
+    "classpath:spring/fixtures/fixtures.xml"
 ])
 class GithubProviderTest extends AbstractTestNGSpringContextTests {
+
+    @Autowired
+    UserDAOImplFixtures githubUserDAOImplFixtures
 
     GithubProvider githubProvider
 
@@ -41,18 +46,33 @@ class GithubProviderTest extends AbstractTestNGSpringContextTests {
 
     RateLimit rateLimitSetSearch
 
+    UserSet userSet
+
+    String login
+
+    PaginationAwareArrayList followersList
+
+    PaginationAwareArrayList followingList
+
     @BeforeMethod
     void setUp() {
         githubJcabiProviderMock = mock GithubJcabiProvider.class
         githubEgitProviderMock = mock GithubEgitProvider.class
         githubKohsukeProviderMock = mock GithubKohsukeProvider.class
         rateLimitDAOImplMock = mock RateLimitDAOImpl.class
+        userSet = mock UserSet.class
+        rateLimitSetCore = mock RateLimit.class
+        rateLimitSetSearch = mock RateLimit.class
+        followersList = mock PaginationAwareArrayList.class
+        followingList = mock PaginationAwareArrayList.class
 
-        rateLimitSetCore = new RateLimit("core", 50, 45, new Date())
-        rateLimitSetSearch = new RateLimit("search", 50, 45, new Date())
+        login = githubUserDAOImplFixtures.getRandomLogin()
 
         when githubJcabiProviderMock.getCoreLimit() thenReturn rateLimitSetCore
         when githubJcabiProviderMock.getSearchLimit() thenReturn rateLimitSetSearch
+        when githubJcabiProviderMock.getUser(login) thenReturn userSet
+        when githubEgitProviderMock.getFollowers(login, 1, 0) thenReturn followersList
+        when githubEgitProviderMock.getFollowing(login, 1, 0) thenReturn followingList
 
         githubProvider = new GithubProvider(
             githubJcabiProviderMock,
@@ -84,17 +104,32 @@ class GithubProviderTest extends AbstractTestNGSpringContextTests {
 
     @Test
     void getUser() {
-        //
+        // exercise
+        UserSet userSetReturned = githubProvider.getUser(login)
+
+        // assertion
+        verify(githubJcabiProviderMock).getUser(login)
+        Assert.assertEquals userSetReturned, userSet
     }
 
     @Test
     void getFollowers() {
-        //
+        // exercise
+        PaginationAwareArrayList<UserSet> followersListReturned = githubProvider.getFollowers(login, 1, 0)
+
+        // assertion
+        verify(githubEgitProviderMock).getFollowers(login, 1, 0)
+        Assert.assertEquals followersListReturned, followersList
     }
 
     @Test
     void getFollowing() {
-        //
+        // exercise
+        PaginationAwareArrayList<UserSet> followingListReturned = githubProvider.getFollowing(login, 1, 0)
+
+        // assertion
+        verify(githubEgitProviderMock).getFollowing(login, 1, 0)
+        Assert.assertEquals followingListReturned, followingList
     }
 
 }

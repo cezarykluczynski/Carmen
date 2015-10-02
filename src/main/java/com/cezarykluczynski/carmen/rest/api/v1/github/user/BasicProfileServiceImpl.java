4f4357a231532b0969efc2206b70399fb7779c02
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2015-10-02 22:26:22 +0200

    Feeding real data to basicProfile API endpoint

--- src/main/java/com/cezarykluczynski/carmen/rest/api/v1/github/user/BasicProfileServiceImpl.java
+++ src/main/java/com/cezarykluczynski/carmen/rest/api/v1/github/user/BasicProfileServiceImpl.java
@@ -1,17 +1,40 @@
 package com.cezarykluczynski.carmen.rest.api.v1.github.user;
 
+import com.cezarykluczynski.carmen.dao.github.UserDAOImpl;
 import com.cezarykluczynski.carmen.pojo.rest.api.v1.github.user.BasicProfilePOJO;
+import com.cezarykluczynski.carmen.model.github.User;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.ws.rs.PathParam;
+import javax.ws.rs.NotFoundException;
 
 @Service("basicProfileService")
 public class BasicProfileServiceImpl implements BasicProfileService {
 
+    @Autowired
+    UserDAOImpl githubUserDAOImpl;
+
     @Override
     public BasicProfilePOJO get(String login) {
-        return new BasicProfilePOJO();
+        User userEntity = githubUserDAOImpl.findByLogin(login);
+
+        if (null == userEntity) {
+            throw new NotFoundException();
+        }
+
+        return new BasicProfilePOJO(
+            userEntity.getLogin(),
+            userEntity.getName(),
+            userEntity.getAvatarUrl(),
+            userEntity.getCompany(),
+            userEntity.getBlog(),
+            userEntity.getLocation(),
+            userEntity.getEmail(),
+            userEntity.getBio(),
+            userEntity.getHireable()
+        );
     }
 
 }
--- src/main/java/com/cezarykluczynski/carmen/rest/pojo/api/v1/github/user/BasicProfilePOJO.java
+++ src/main/java/com/cezarykluczynski/carmen/rest/pojo/api/v1/github/user/BasicProfilePOJO.java
@@ -12,39 +12,33 @@ public class BasicProfilePOJO {
         String login,
         String name,
         String avatarUrl,
-        String type,
-        boolean siteAdmin,
         String company,
         String blog,
         String location,
         String email,
+        String bio,
         boolean hireable
     ) {
         this.login = login;
         this.name = name;
         this.avatarUrl = avatarUrl;
-        this.type = type;
-        this.siteAdmin = siteAdmin;
         this.company = company;
         this.blog = blog;
         this.location = location;
         this.email = email;
+        this.bio = bio;
         this.hireable = hireable;
     }
 
     private String login;
     private String name;
-    private boolean requested;
-    private boolean optOut;
     private String avatarUrl;
-    private String type;
-    private boolean siteAdmin;
     private String company;
     private String  blog;
     private String  location;
     private String  email;
-    private boolean hireable;
     private String bio;
+    private boolean hireable;
 
     public String getLogin() {
         return login;
@@ -54,24 +48,8 @@ public class BasicProfilePOJO {
         return name;
     }
 
-    public boolean getRequested() {
-        return requested;
-    }
-
-    public boolean getOptOut() {
-        return optOut;
-    }
-
     public String getAvatarUrl() {
         return avatarUrl;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public boolean getSiteAdmin() {
-        return siteAdmin;
     }
 
     public String getCompany() {
@@ -90,12 +68,12 @@ public class BasicProfilePOJO {
         return email;
     }
 
-    public boolean getHireable() {
-        return hireable;
-    }
-
     public String getBio() {
         return bio;
+    }
+
+    public boolean getHireable() {
+        return hireable;
     }
 
 }

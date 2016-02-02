ae97563d89318d00856d205c9b2aac3da66067b5
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-02-02 19:49:02 +0100

    Cassandra migrations moved to CQL files

--- src/main/java/com/cezarykluczynski/carmen/db/CassandraMigrations.java
+++ src/main/java/com/cezarykluczynski/carmen/db/CassandraMigrations.java
@@ -1,83 +1,54 @@
 package com.cezarykluczynski.carmen.db;
 
+import com.contrastsecurity.cassandra.migration.CassandraMigration;
 import com.netflix.astyanax.*;
+import com.netflix.astyanax.Keyspace;
 import com.netflix.astyanax.impl.*;
-import com.netflix.astyanax.model.Column;
-import com.netflix.astyanax.model.ColumnFamily;
-import com.netflix.astyanax.model.CqlResult;
-import com.netflix.astyanax.model.Rows;
-import com.netflix.astyanax.serializers.StringSerializer;
-import com.netflix.astyanax.serializers.UUIDSerializer;
 import com.netflix.astyanax.connectionpool.*;
 import com.netflix.astyanax.connectionpool.impl.*;
 import com.netflix.astyanax.thrift.ThriftFamilyFactory;
-import com.netflix.astyanax.util.RangeBuilder;
-import com.netflix.astyanax.thrift.model.ThriftRowImpl;
 import com.netflix.astyanax.connectionpool.exceptions.ConnectionException;
-import com.netflix.astyanax.model.ColumnList;
 
 import com.google.common.collect.ImmutableMap;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.sql.Date;
 import java.util.Properties;
-import java.util.UUID;
-import java.util.Iterator;
 
 class CassandraMigrations {
 
+    private static String contactpoints;
+    private static Integer thriftPort;
+    private static Integer port;
+    private static String cluster;
+    private static final String githubSocialStatsKeyspaceName = "github_social_stats";
+    private static String version;
+
     public static void main(String[] args) throws ConnectionException, IOException {
-        System.out.println("Cassandra migrations started.");
+        System.out.println("Carmen: Cassandra migrations started.");
 
-        String contactpoints = null;
-        Integer port = null;
-        String cluster = null;
-        String keyspaceName = null;
-        String version = null;
+        configure();
+        createGithubSocialStatsKeyspace();
+        migrateGithubSocialStatsKeyspace();
+        System.exit(0);
+    }
 
+    private static void configure() throws IOException {
         try {
             Properties properties = new Properties();
             InputStream inputStream = CassandraMigrations.class.getClassLoader().getResourceAsStream("config.properties");
             properties.load(inputStream);
 
             contactpoints = properties.getProperty("cassandra.contactpoints");
-            port = Integer.parseInt(properties.getProperty("cassandra.thrift_port"));
+            thriftPort = Integer.parseInt(properties.getProperty("cassandra.thrift_port"));
+            port = Integer.parseInt(properties.getProperty("cassandra.port"));
             cluster = properties.getProperty("cassandra.cluster");
-            keyspaceName = properties.getProperty("cassandra.keyspace");
-            version = (String) properties.getProperty("cassandra.version");
+            version = properties.getProperty("cassandra.version");
         } catch (IOException ioe) {
             System.out.println("Carmen: config.properties for Cassandra not found or incomplete.");
 
             throw ioe;
         }
-
-        AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
-            .forCluster(cluster)
-            .forKeyspace(keyspaceName)
-            .withAstyanaxConfiguration(new AstyanaxConfigurationImpl()
-                .setTargetCassandraVersion(version)
-                .setCqlVersion("3.1.1")
-                .setDiscoveryType(NodeDiscoveryType.NONE)
-            )
-            .withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("MyConnectionPool")
-                .setPort(port)
-                .setMaxConnsPerHost(1)
-                .setSeeds(contactpoints + ":" + Integer.toString(port)) // TODO: allow multiple contactpoints
-            )
-            .withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
-            .buildKeyspace(ThriftFamilyFactory.getInstance());
-
-        context.start();
-        Keyspace keyspace = context.getClient();
-
-        createKeyspace(keyspace);
-        createMigrationsTable(keyspace);
-        migrateFollowersAndFollowees(keyspace);
-        migrateFollowingBeingFollowees(keyspace);
-
-        context.shutdown();
-        System.exit(0);
     }
 
     private static void createKeyspace(Keyspace keyspace) throws ConnectionException {
@@ -104,58 +75,46 @@ class CassandraMigrations {
         }
     }
 
-    private static void createMigrationsTable(Keyspace keyspace) throws ConnectionException {
-        String keyspaceName = keyspace.getKeyspaceName();
+    private static void createGithubSocialStatsKeyspace() throws ConnectionException {
+        AstyanaxContext<Keyspace> context = new AstyanaxContext.Builder()
+                .forCluster(cluster)
+                .forKeyspace(githubSocialStatsKeyspaceName)
+                .withAstyanaxConfiguration(new AstyanaxConfigurationImpl()
+                                .setTargetCassandraVersion(version)
+                                .setCqlVersion("3.1.1")
+                                .setDiscoveryType(NodeDiscoveryType.NONE)
+                )
+                .withConnectionPoolConfiguration(new ConnectionPoolConfigurationImpl("MyConnectionPool")
+                        .setPort(thriftPort)
+                        .setMaxConnsPerHost(3)
+                        .setSeeds(contactpoints + ":" + Integer.toString(thriftPort)) // TODO: allow multiple contactpoints
+                )
+                .withConnectionPoolMonitor(new CountingConnectionPoolMonitor())
+                .buildKeyspace(ThriftFamilyFactory.getInstance());
 
-        ColumnFamily<UUID, String> SCHEMA_MIGRATIONS = ColumnFamily.newColumnFamily(
-            "schema_migrations",
-            UUIDSerializer.get(),
-            StringSerializer.get()
-        );
+        context.start();
+        Keyspace keyspace = context.getClient();
 
-        OperationResult<CqlResult<UUID, String>> result
-            = keyspace.prepareQuery(SCHEMA_MIGRATIONS)
-                .withCql("CREATE TABLE IF NOT EXISTS " + keyspaceName  +".schema_migrations " +
-                    "(version int, name varchar, created timestamp, PRIMARY KEY(version));")
-                .execute();
+        createKeyspace(keyspace);
+        context.shutdown();
     }
 
-    private static void migrateFollowersAndFollowees(Keyspace keyspace) throws ConnectionException {
-        String keyspaceName = keyspace.getKeyspaceName();
+    private static void migrateGithubSocialStatsKeyspace() {
+        String[] scriptsLocations = {
+                "filesystem:src/main/java/com/cezarykluczynski/carmen/db/migration/cassandra/github_social_stats/"
+        };
 
-        ColumnFamily<UUID, String> SCHEMA_MIGRATIONS = ColumnFamily.newColumnFamily(
-            "followers_and_followees",
-            UUIDSerializer.get(),
-            StringSerializer.get()
-        );
+        com.contrastsecurity.cassandra.migration.config.Keyspace keyspace =
+                new com.contrastsecurity.cassandra.migration.config.Keyspace();
+        keyspace.setName(githubSocialStatsKeyspaceName);
+        com.contrastsecurity.cassandra.migration.config.Cluster cluster = keyspace.getCluster();
+        cluster.setContactpoints(contactpoints);
+        cluster.setPort(port);
 
-        keyspace.prepareQuery(SCHEMA_MIGRATIONS)
-            .withCql("CREATE TABLE IF NOT EXISTS " + keyspaceName  +".followers_and_followees " +
-                "(id uuid, user_id int, followers_count int, followees_count int, shared_count int, PRIMARY key(id));")
-            .execute();
-
-        keyspace.prepareQuery(SCHEMA_MIGRATIONS)
-                .withCql("CREATE INDEX IF NOT EXISTS ON " + keyspaceName + ".followers_and_followees (user_id);")
-                .execute();
-    }
-
-    private static void migrateFollowingBeingFollowees(Keyspace keyspace) throws ConnectionException {
-    	String keyspaceName = keyspace.getKeyspaceName();
-
-        ColumnFamily<UUID, String> SCHEMA_MIGRATIONS = ColumnFamily.newColumnFamily(
-            "followeers_being_followees",
-            UUIDSerializer.get(),
-            StringSerializer.get()
-        );
-
-        keyspace.prepareQuery(SCHEMA_MIGRATIONS)
-            .withCql("CREATE TABLE IF NOT EXISTS " + keyspaceName  +".followers_being_followees " +
-                "(id uuid, user_id int, follower_being_followee int, PRIMARY key(id));")
-            .execute();
-
-        keyspace.prepareQuery(SCHEMA_MIGRATIONS)
-            .withCql("CREATE INDEX IF NOT EXISTS ON " + keyspaceName + ".followers_being_followees (user_id);")
-            .execute();
+        CassandraMigration cassandraMigration = new CassandraMigration();
+        cassandraMigration.getConfigs().setScriptsLocations(scriptsLocations);
+        cassandraMigration.setKeyspace(keyspace);
+        cassandraMigration.migrate();
     }
 
 }

0b41b4dad0b1fc497da56a32f7b40a5967277be2
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-06-02 21:30:49 +0200

    Trying different quotation marks in CommitLogSinceCommand

--- src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
+++ src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
@@ -1,11 +1,14 @@
 package com.cezarykluczynski.carmen.vcs.git.util;
 
+import com.cezarykluczynski.carmen.util.DateUtil;
+
 import java.util.Date;
 
 public class GitLogSinceCommand extends GitCommand {
 
-    public GitLogSinceCommand(String cloneDirectory, Date since) {
-        super("log --oneline");
+    public GitLogSinceCommand(String directory, Date since) {
+        super("log --pretty='%H %ct %ae' --date-order --reverse --since='" +
+                DateUtil.toGitReadableDateTime(since) + "'");
     }
 
 }

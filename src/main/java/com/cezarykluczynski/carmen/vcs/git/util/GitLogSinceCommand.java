c1588c8a9edfed385d5e6e38c966b8aa81a6c27e
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-06-02 21:45:17 +0200

    Checking when will GitLogSinceCommand pass

--- src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
+++ src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
@@ -1,14 +1,11 @@
 package com.cezarykluczynski.carmen.vcs.git.util;
 
-import com.cezarykluczynski.carmen.util.DateUtil;
-
 import java.util.Date;
 
 public class GitLogSinceCommand extends GitCommand {
 
     public GitLogSinceCommand(String directory, Date since) {
-        super("log --pretty='%H %ct %ae' --date-order --reverse --since='" +
-                DateUtil.toGitReadableDateTime(since) + "'");
+        super("log --date-order --reverse");
     }
 
 }

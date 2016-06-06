e13b28eb73ab383567f105f5609dbdcdcf3cd996
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-06-06 21:16:04 +0200

    Debugging on CircleCI

--- src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
+++ src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
@@ -1,6 +1,5 @@
 package com.cezarykluczynski.carmen.vcs.git.util;
 
-import com.cezarykluczynski.carmen.util.DateUtil;
 import com.cezarykluczynski.carmen.util.exec.Command;
 
 import java.util.Date;
@@ -8,8 +7,7 @@ import java.util.Date;
 public class GitLogSinceCommand extends Command {
 
     public GitLogSinceCommand(String directory, Date since) {
-        super("pushd " + directory + " && git log --pretty=%H,%ct,%ae --date-order --reverse --since=" +
-                DateUtil.toGitReadableDateTime(since) + " && popd");
+        super("pushd " + directory);
     }
 
 }

3a07c826189fd8585eeff6c20afba1c93d9e159f
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-06-26 18:00:42 +0200

    GitLogSinceCommand modified to allow both since and before dates to be passed

--- src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
+++ src/main/java/com/cezarykluczynski/carmen/vcs/git/util/GitLogSinceCommand.java
@@ -2,19 +2,33 @@ package com.cezarykluczynski.carmen.vcs.git.util;
 
 import com.cezarykluczynski.carmen.util.DateUtil;
 import com.cezarykluczynski.carmen.util.exec.command.ProcessBuilderCommand;
+import com.google.common.collect.Lists;
 
 import java.util.Date;
+import java.util.List;
 
 public class GitLogSinceCommand extends ProcessBuilderCommand {
 
-    public GitLogSinceCommand(String directory, Date since) {
-        super(getCommandParts(since), directory);
+    public GitLogSinceCommand(String directory, Date since, Date before) {
+        super(getCommandParts(since, before), directory);
     }
 
-    private static String[] getCommandParts(Date since) {
-        String[] commandParts = {"git",  "log",  "--pretty=%H,%ct,%ae",  "--date-order", "--reverse", "--since=" +
-                DateUtil.toGitReadableDateTime(since)};
-        return commandParts;
+    private static String[] getCommandParts(Date since, Date before) {
+        List<String> commandParts = Lists.newArrayList("git", "log", "--pretty=%H,%ct,%ae", "--date-order",
+            "--reverse");
+
+        if (since != null) {
+            commandParts.add("--since=" + DateUtil.toGitReadableDateTime(since));
+        }
+
+        if (before != null) {
+            commandParts.add("--before=" + DateUtil.toGitReadableDateTime(before));
+        }
+
+        String[] commandPartsArray = new String[commandParts.size()];
+        commandPartsArray = commandParts.toArray(commandPartsArray);
+
+        return commandPartsArray;
     }
 
 }

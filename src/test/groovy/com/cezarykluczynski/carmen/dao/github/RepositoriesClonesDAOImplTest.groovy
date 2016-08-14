f7d070ecb82214c1eb64b932766a2155d82ad673
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-08-14 21:40:26 +0200

    RepositoriesClonesDAOImplTest moved to Spock

--- src/test/groovy/com/cezarykluczynski/carmen/dao/github/RepositoriesClonesDAOImplTest.groovy
+++ src/test/groovy/com/cezarykluczynski/carmen/dao/github/RepositoriesClonesDAOImplTest.groovy
@@ -1,6 +1,6 @@
 package com.cezarykluczynski.carmen.dao.github
 
-import com.cezarykluczynski.carmen.configuration.TestableApplicationConfiguration
+import com.cezarykluczynski.carmen.IntegrationTest
 import com.cezarykluczynski.carmen.model.github.Repository
 import com.cezarykluczynski.carmen.model.github.RepositoryClone
 import com.cezarykluczynski.carmen.testutil.DateFactory
@@ -8,18 +8,8 @@ import com.cezarykluczynski.carmen.util.filesystem.Directory
 import com.cezarykluczynski.carmen.vcs.server.Server
 import com.cezarykluczynski.carmen.vcs.server.ServerTest
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.testng.AbstractTestNGSpringContextTests
-import org.springframework.test.context.web.WebAppConfiguration
-import org.testng.Assert
-import org.testng.annotations.Test
 
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.when
-
-@ContextConfiguration(classes = TestableApplicationConfiguration.class)
-@WebAppConfiguration
-class RepositoriesClonesDAOImplTest extends AbstractTestNGSpringContextTests {
+class RepositoriesClonesDAOImplTest extends IntegrationTest {
 
     @Autowired
     RepositoriesClonesDAO repositoriesClonesDAO
@@ -33,29 +23,31 @@ class RepositoriesClonesDAOImplTest extends AbstractTestNGSpringContextTests {
     @Autowired
     RepositoriesClonesDAOImplFixtures repositoriesClonesDAOImplFixtures
 
-    @Test
-    void entityCanBeCreated() {
-        Server server = mock Server.class
-        when server.getServerId() thenReturn ServerTest.SERVER_ID
-        when server.getCloneRoot() thenReturn ServerTest.CLONE_ROOT
+    def "entity can be created"() {
+        given:
+        Server server = Mock(Server) {
+            getServerId() >> ServerTest.SERVER_ID
+            getCloneRoot() >> ServerTest.CLONE_ROOT
+        }
 
         Repository repositoryEntity = new Repository()
         repositoryEntity.setFullName "username/repository"
         githubRepositoriesDAO.create repositoryEntity
 
+        when:
         RepositoryClone repositoryCloneEntity = repositoriesClonesDAO.createStubEntity(server, repositoryEntity)
 
-        Assert.assertTrue repositoryCloneEntity instanceof RepositoryClone
-        Assert.assertNotNull repositoryCloneEntity.getId()
+        then:
+        repositoryCloneEntity instanceof RepositoryClone
+        repositoryCloneEntity.id != null
 
-        // teardown
+        cleanup:
         githubRepositoriesDAOImplFixtures.deleteRepositoryEntity repositoryEntity
         Directory.delete server.getCloneRoot()
     }
 
-    @Test
-    void "find repository clone with commits to persists when there are clones that were never persisted"() {
-        // setup
+    def "finds repository clone with commits to persists when there are clones that were never persisted"() {
+        given:
         RepositoryClone repositoryClone1 = repositoriesClonesDAOImplFixtures
                 .createWithUpdatedAndCommitsStatisticsUntil(DateFactory.of(2010, 1, 1), DateFactory.of(2010, 2, 2))
         RepositoryClone repositoryClone2 = repositoriesClonesDAOImplFixtures
@@ -65,34 +57,35 @@ class RepositoriesClonesDAOImplTest extends AbstractTestNGSpringContextTests {
         RepositoryClone repositoryClone4 = repositoriesClonesDAOImplFixtures
                 .createWithUpdatedAndCommitsStatisticsUntil(DateFactory.of(2011, 1, 1), null)
 
-        // exercise
+        when:
         RepositoryClone repositoryCloneFound = repositoriesClonesDAO.findRepositoryCloneWithCommitsToPersist()
 
-        // assertion
-        Assert.assertNotNull repositoryCloneFound
-        Assert.assertEquals repositoryCloneFound.getId(), repositoryClone4.getId()
+        then:
+        repositoryCloneFound != null
+        repositoryCloneFound.getId() == repositoryClone4.getId()
 
+        cleanup:
         repositoriesClonesDAOImplFixtures.delete repositoryClone1
         repositoriesClonesDAOImplFixtures.delete repositoryClone2
         repositoriesClonesDAOImplFixtures.delete repositoryClone3
         repositoriesClonesDAOImplFixtures.delete repositoryClone4
     }
 
-    @Test
-    void "find repository clone with commits to persists when there are no clones that were never persisted"() {
-        // setup
+    def "finds repository clone with commits to persists when there are no clones that were never persisted"() {
+        given:
         RepositoryClone repositoryClone1 = repositoriesClonesDAOImplFixtures
                 .createWithUpdatedAndCommitsStatisticsUntil(DateFactory.of(2010, 1, 1), DateFactory.of(2010, 2, 2))
         RepositoryClone repositoryClone2 = repositoriesClonesDAOImplFixtures
                 .createWithUpdatedAndCommitsStatisticsUntil(DateFactory.of(2011, 1, 1), DateFactory.of(2011, 2, 2))
 
-        // exercise
+        when:
         RepositoryClone repositoryCloneFound = repositoriesClonesDAO.findRepositoryCloneWithCommitsToPersist()
 
-        // assertion
-        Assert.assertNotNull repositoryCloneFound
-        Assert.assertEquals repositoryCloneFound.getId(), repositoryClone1.getId()
+        then:
+        repositoryCloneFound != null
+        repositoryCloneFound.getId() == repositoryClone1.getId()
 
+        cleanup:
         repositoriesClonesDAOImplFixtures.delete repositoryClone1
         repositoriesClonesDAOImplFixtures.delete repositoryClone2
     }

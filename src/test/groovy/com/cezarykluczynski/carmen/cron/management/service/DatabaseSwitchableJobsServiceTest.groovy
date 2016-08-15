be8af4d2d87df0f87f02af23e0c82aadc70967b7
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-08-15 22:38:04 +0200

    DatabaseSwitchableJobsServiceTest moved to Spock

--- src/test/groovy/com/cezarykluczynski/carmen/cron/management/service/DatabaseSwitchableJobsServiceTest.groovy
+++ src/test/groovy/com/cezarykluczynski/carmen/cron/management/service/DatabaseSwitchableJobsServiceTest.groovy
@@ -4,19 +4,9 @@ import com.beust.jcommander.internal.Lists
 import com.cezarykluczynski.carmen.cron.management.dto.DatabaseSwitchableJobDTO
 import com.cezarykluczynski.carmen.dao.pub.CronsDAO
 import com.cezarykluczynski.carmen.model.pub.Cron
-import org.mockito.ArgumentCaptor
-import org.testng.Assert
-import org.testng.annotations.BeforeMethod
-import org.testng.annotations.Test
+import spock.lang.Specification
 
-import static org.mockito.Matchers.any
-import static org.mockito.Mockito.doNothing
-import static org.mockito.Mockito.mock
-import static org.mockito.Mockito.never
-import static org.mockito.Mockito.when
-import static org.mockito.Mockito.verify
-
-class DatabaseSwitchableJobsServiceTest {
+class DatabaseSwitchableJobsServiceTest extends Specification {
 
     private static final String NAME_1 = "DatabaseSwitchableJobAnnotatedMock"
     private static final String NAME_2 = "DatabaseSwitchableJobAnnotatedMock2"
@@ -34,141 +24,142 @@ class DatabaseSwitchableJobsServiceTest {
 
     private Cron cronMock3
 
-    @BeforeMethod
-    void setup() {
-        cronsDAOMock = mock CronsDAO
-        jobListProviderMock = mock DatabaseSwitchableJobListProvider
-        cronMock = mock Cron
-        when cronMock.getName() thenReturn NAME_1
-        cronMock3 = mock Cron
-        when cronMock3.getName() thenReturn NAME_3
-        when jobListProviderMock.getDatabaseSwitchableJobsClasses() thenReturn Lists
-                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
-        doNothing().when(cronMock).setEnabled true
-        doNothing().when(cronMock).setEnabled false
-        doNothing().when(cronsDAOMock).update cronMock
+    def setup() {
+        cronsDAOMock = Mock CronsDAO
+        jobListProviderMock = Mock DatabaseSwitchableJobListProvider
+        cronMock = Mock Cron
+        cronMock.getName() >> NAME_1
+        cronMock3 = Mock Cron
+        cronMock3.getName() >> NAME_3
         cronList = Lists.newArrayList cronMock
-        when cronsDAOMock.findAll() thenReturn cronList
-        when(cronsDAOMock.create(any(Cron.class))).thenReturn null
-        doNothing().when(cronsDAOMock).delete(any(Cron.class))
         service = new DatabaseSwitchableJobsService(cronsDAOMock, jobListProviderMock)
     }
 
-    @Test
-    void getAll() {
-        // exercise
+    def "gets all cron jobs"() {
+        given:
+        cronsDAOMock.findAll() >> cronList
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
+
+        when:
         List<DatabaseSwitchableJobDTO> list = service.getAll()
 
-        // assertion
-        Assert.assertEquals list.size(), 1
-        Assert.assertEquals list.get(0).getName(), NAME_1
+        then:
+        list.size() == 1
+        list.get(0).getName() == NAME_1
     }
 
-    @Test
-    void updateList() {
-        // setup
-        when jobListProviderMock.getDatabaseSwitchableJobsClasses() thenReturn Lists
+    def "job list is updated"() {
+        given:
+        cronsDAOMock.findAll() >> cronList
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
                 .newArrayList(DatabaseSwitchableJobAnnotatedMock.class, DatabaseSwitchableJobAnnotatedMock2.class)
         cronList = Lists.newArrayList(cronMock, cronMock3)
-        when cronsDAOMock.findAll() thenReturn cronList
-        when cronsDAOMock.findByName(NAME_3) thenReturn Lists.newArrayList(cronMock3)
+        cronsDAOMock.findByName(NAME_3) >> Lists.newArrayList(cronMock3)
 
-        // exercise
+        when:
         service.updateList()
 
-        // assertion
-        verify(cronsDAOMock).findAll()
-        ArgumentCaptor<Cron> cronArgumentCaptor = ArgumentCaptor.forClass Cron.class
-        verify(cronsDAOMock).create(cronArgumentCaptor.capture())
-        cronArgumentCaptor.allValues.get(0).name == NAME_2
-        verify(cronsDAOMock).delete(cronArgumentCaptor.capture())
-        cronArgumentCaptor.allValues.get(0).name == NAME_3
+        then:
+        1 * cronsDAOMock.findAll() >> cronList
+        1 * cronsDAOMock.delete(_) >> { args ->
+            Cron cron = (Cron) args[0]
+            assert cron.name == NAME_3
+        }
+        1 * cronsDAOMock.create(_) >> { args ->
+            Cron cron = (Cron) args[0]
+            assert cron.name == NAME_2
+        }
     }
 
-
-    @Test
-    void enable() {
-        // setup
+    def "job is enabled"() {
+        given:
+        cronsDAOMock.findAll() >> cronList
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
         DatabaseSwitchableJobDTO dto = createDatabaseSwitchableJobDTO()
 
-        // exercise
+        when:
         service.enable dto
 
-        // assertion
-        verify(cronMock).setEnabled true
-        verify(cronMock, never()).setEnabled false
-        verify(cronsDAOMock).update cronMock
+        then:
+        1 * cronMock.setEnabled(true)
+        0 * cronMock.setEnabled(false)
+        1 * cronsDAOMock.update(cronMock)
     }
 
-
-    @Test
-    void "does not enable when entity is missing"() {
-        // setup
-        when cronsDAOMock.findAll() thenReturn Lists.newArrayList()
+    def "does not enable when entity is missing"() {
+        given:
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
+        cronsDAOMock.findAll() >> Lists.newArrayList()
         DatabaseSwitchableJobDTO dto = createDatabaseSwitchableJobDTO()
 
-        // exercise
+        when:
         service.enable dto
 
-        // assertion
-        verify(cronMock, never()).setEnabled true
-        verify(cronMock, never()).setEnabled false
-        verify(cronsDAOMock, never()).update cronMock
+        then:
+        0 * cronMock.setEnabled(_)
+        0 * cronsDAOMock.update(cronMock)
     }
 
-    @Test
-    void disable() {
-        // setup
+    def "cron is being disabled"() {
+        given:
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
+        cronsDAOMock.findAll() >> cronList
         DatabaseSwitchableJobDTO dto = createDatabaseSwitchableJobDTO()
 
-        // exercise
+        when:
         service.disable dto
 
-        // assertion
-        verify(cronMock).setEnabled false
-        verify(cronMock, never()).setEnabled true
-        verify(cronsDAOMock).update cronMock
+        then:
+        1 * cronMock.setEnabled(false)
+        0 * cronMock.setEnabled(true)
+        1 * cronsDAOMock.update(cronMock)
     }
 
-    @Test
-    void "cron should be enabled when it is not found"() {
-        // setup
-        when cronsDAOMock.findAll() thenReturn Lists.newArrayList()
+    def "cron should be enabled when it is not found"() {
+        given:
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
+        cronsDAOMock.findAll() >> Lists.newArrayList()
 
-        // exercise
+        when:
         boolean enabled = service.isEnabledOrNotDatabaseSwitchable NAME_1
 
-        // assertion
-        Assert.assertTrue enabled
+        then:
+        enabled
     }
 
-    @Test
-    void "cron should be enabled when it is enabled"() {
-        // setup
-        when cronsDAOMock.findAll() thenReturn Lists.newArrayList(
+    def "cron should be enabled when it is enabled"() {
+        given:
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
+        cronsDAOMock.findAll() >> Lists.newArrayList(
                 new Cron(name: NAME_1, enabled: true)
         )
 
-        // exercise
+        when:
         boolean enabled = service.isEnabledOrNotDatabaseSwitchable NAME_1
 
-        // assertion
-        Assert.assertTrue enabled
+        then:
+        enabled
     }
 
-
-    @Test
-    void "cron should be disabled when it is not enabled"() {
-        // setup
-        when cronsDAOMock.findAll() thenReturn Lists.newArrayList(
+    def "cron should be disabled when it is not enabled"() {
+        given:
+        jobListProviderMock.getDatabaseSwitchableJobsClasses() >> Lists
+                .newArrayList(DatabaseSwitchableJobAnnotatedMock.class)
+        cronsDAOMock.findAll() >> Lists.newArrayList(
                 new Cron(name: NAME_1, enabled: false)
         )
 
-        // exercise
+        when:
         boolean enabled = service.isEnabledOrNotDatabaseSwitchable NAME_1
 
-        // assertion
-        Assert.assertFalse enabled
+        then:
+        !enabled
     }
 
     private static DatabaseSwitchableJobDTO createDatabaseSwitchableJobDTO() {

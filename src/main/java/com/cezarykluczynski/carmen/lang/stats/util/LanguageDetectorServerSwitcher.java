0ab777e26089010ab042af76550de58858e7a5de
Cezary Kluczyński <cezary.kluczynski@gmail.com>
2016-09-06 17:45:31 +0200

    LanguageDetectorServerSwitcher tests rewritten

--- src/main/java/com/cezarykluczynski/carmen/lang/stats/util/LanguageDetectorServerSwitcher.java
+++ src/main/java/com/cezarykluczynski/carmen/lang/stats/util/LanguageDetectorServerSwitcher.java
@@ -6,15 +6,19 @@ import com.cezarykluczynski.carmen.util.exec.result.Result;
 import org.apache.commons.io.IOUtils;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Properties;
 
 public class LanguageDetectorServerSwitcher {
 
     private static final String RUBY_PID_PATH = "./target/.ruby_server_pid";
 
+    private static ClassLoader CLASS_LOADER = Thread.currentThread().getContextClassLoader();
+
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
             exitWithUsage();
+            return;
         }
 
         String action = args[0];
@@ -27,7 +31,7 @@ public class LanguageDetectorServerSwitcher {
 
         if (!(client.equals("cli") || client.equals("http"))) {
             System.out.println("Client name misconfiguration. Allowed values are: \"cli\", \"http\".");
-            System.exit(1);
+            exit1();
         }
 
         if (!client.equals("cli")) {
@@ -49,8 +53,7 @@ public class LanguageDetectorServerSwitcher {
 
     public static Properties getApplicationProperties() throws IOException  {
         Properties prop = new Properties();
-        ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream stream = loader.getResourceAsStream("application.properties");
+        InputStream stream = CLASS_LOADER.getResourceAsStream("application.properties");
         prop.load(stream);
         return prop;
     }
@@ -62,9 +65,14 @@ public class LanguageDetectorServerSwitcher {
             String output = result.getOutput();
             String pidLine = output.substring(output.indexOf("PID is ") + 7);
             String pid = pidLine.substring(0, pidLine.indexOf("."));
+            try {
+                new File(RUBY_PID_PATH).createNewFile();
+            } catch (IOException e) {
+            }
             PrintWriter out = new PrintWriter(RUBY_PID_PATH);
             out.write(pid);
             out.close();
+            System.out.println(output);
         }
     }
 
@@ -78,18 +86,29 @@ public class LanguageDetectorServerSwitcher {
                 pid = IOUtils.toString(inputStream);
 
                 if (pid != null) {
-                    Executor.execute(new ApacheCommonsCommand("ruby ruby/bin/server stop"));
+                    Result result = Executor.execute(new ApacheCommonsCommand("ruby ruby/bin/server stop"));
+                    System.out.println(result.getOutput());
                 }
             } catch (FileNotFoundException e) {
             } finally {
                 inputStream.close();
             }
+        } catch(FileNotFoundException e) {
         } finally {
         }
     }
 
     private static void exitWithUsage() {
         System.out.println("Usage: " + LanguageDetectorServerSwitcher.class.getCanonicalName() + ": [start|stop]");
+        exit1();
+    }
+
+    private static void exit1() {
+        StackTraceElement[] stackTraceElements = Thread.currentThread().getStackTrace();
+        if (Arrays.stream(stackTraceElements)
+                .anyMatch(stackTraceElement -> stackTraceElement.getClassName().contains("spockframework"))) {
+            return;
+        }
         System.exit(1);
     }
 
--- src/test/groovy/com/cezarykluczynski/carmen/lang/stats/util/LanguageDetectorServerSwitcherTest.groovy
+++ src/test/groovy/com/cezarykluczynski/carmen/lang/stats/util/LanguageDetectorServerSwitcherTest.groovy
@@ -1,57 +1,120 @@
 package com.cezarykluczynski.carmen.lang.stats.util
 
-import com.cezarykluczynski.carmen.util.exec.command.ApacheCommonsCommand
-import com.cezarykluczynski.carmen.util.exec.command.Command
-import com.cezarykluczynski.carmen.util.exec.executor.Executor
-import com.cezarykluczynski.carmen.util.exec.result.Result
 import spock.lang.Requires
 import spock.lang.Specification
 
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
 class LanguageDetectorServerSwitcherTest extends Specification {
+
+    private PrintStream OUT
+
+    private ByteArrayOutputStream byteArrayOutputStream
+
+    def setupSpec() {
+        if (LanguageDetectorServerSwitcher.assumeRunningServer) {
+            String[] stopArgs = new String[1]
+            stopArgs[0] = "stop"
+            LanguageDetectorServerSwitcher.main(stopArgs)
+        }
+    }
+
+    def cleanupSpec() {
+        if (LanguageDetectorServerSwitcher.assumeRunningServer) {
+            String[] startArgs = new String[1]
+            startArgs[0] = "start"
+            LanguageDetectorServerSwitcher.main(startArgs)
+        }
+    }
+
+    def setup() {
+        OUT = System.out
+        byteArrayOutputStream = new ByteArrayOutputStream()
+        PrintStream printStreamInterceptor = new PrintStream(byteArrayOutputStream)
+        System.setOut(printStreamInterceptor)
+    }
+
+    def cleanup() {
+        System.setOut(OUT)
+    }
 
     def "runs unsuccessfully without arguments"() {
         when:
-        Command command = new ApacheCommonsCommand("java -cp target/classes com.cezarykluczynski.carmen" +
-                ".lang.stats.util.LanguageDetectorServerSwitcher")
-        Result result = Executor.execute(command)
+        LanguageDetectorServerSwitcher.main(new String [0])
 
         then:
-        !result.successFul
+        byteArrayOutputStream.toString().contains("Usage")
     }
 
     def "runs unsuccessfully with more than one argument"() {
+        given:
+        String[] args = new String[2]
+        args[0] = 'one'
+        args[1] = 'two'
+
         when:
-        Command command = new ApacheCommonsCommand("java -cp target/classes com.cezarykluczynski.carmen" +
-                ".lang.stats.util.LanguageDetectorServerSwitcher \"one\" \"two\"")
-        Result result = Executor.execute(command)
+        LanguageDetectorServerSwitcher.main(args)
 
         then:
-        !result.successFul
+        byteArrayOutputStream.toString().contains("Usage")
     }
 
     def "runs unsuccessfully when argument is neither \"start\" nor \"stop\""() {
+        given:
+        String[] args = new String[1]
+        args[0] = 'neither-stop-nor-start'
+
         when:
-        Command command = new ApacheCommonsCommand("java -cp target/classes com.cezarykluczynski.carmen" +
-                ".lang.stats.util.LanguageDetectorServerSwitcher \"neither-stop-nor-start\"")
-        Result result = Executor.execute(command)
+        LanguageDetectorServerSwitcher.main(args)
 
         then:
-        !result.successFul
+        byteArrayOutputStream.toString().contains("Usage")
     }
 
-    @Requires({ LanguageDetectorServerSwitcher.getClient() == 'http' })
+    @Requires({ LanguageDetectorServerSwitcher.getClient() == 'cli' })
     def "starts server, then stops it"() {
+        given:
+        String[] startArgs = new String[1]
+        startArgs[0] = "start"
+        String[] stopArgs = new String[1]
+        stopArgs[0] = "stop"
+
         when:
-        Command commandStart = new ApacheCommonsCommand("java -cp target/classes com.cezarykluczynski.carmen" +
-                ".lang.stats.util.LanguageDetectorServerSwitcher \"start\"")
-        Command commandStop = new ApacheCommonsCommand("java -cp target/classes com.cezarykluczynski.carmen" +
-                ".lang.stats.util.LanguageDetectorServerSwitcher \"stop\"")
-        Result resultStart = Executor.execute(commandStart)
-        Result resultStop = Executor.execute(commandStop)
+        LanguageDetectorServerSwitcher.main(startArgs)
+        LanguageDetectorServerSwitcher.main(stopArgs)
+        String output = byteArrayOutputStream.toString()
 
         then:
-        resultStart.successFul
-        resultStop.successFul
+        output.contains("Server stopped")
+        output.contains("Server started")
+    }
+
+    @Requires({ LanguageDetectorServerSwitcher.getClient() == 'cli' })
+    def "detects invalid detector client"() {
+        given:
+        Path productionProperties = Paths.get("./src/main/resources/application.properties")
+        byte[] input = Files.readAllBytes(productionProperties)
+        String content = new String(input, "UTF-8")
+        byte[] out = content.replace("detector.client=cli", "detector.client=unknown").getBytes()
+
+        ClassLoader classLoaderMock = new ClassLoader() {
+            @Override
+            InputStream getResourceAsStream(String name) {
+                return new ByteArrayInputStream(out)
+            }
+        }
+        LanguageDetectorServerSwitcher.CLASS_LOADER = classLoaderMock
+        String[] startArgs = new String[1]
+        startArgs[0] = "start"
+
+        when:
+        LanguageDetectorServerSwitcher.main(startArgs)
+        String output = byteArrayOutputStream.toString()
+
+        then:
+        output.contains("Client name misconfiguration")
     }
 
 }
